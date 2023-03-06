0e67b0cecc3ee4968821c7c527f13f2e473bff00
senior_Ori <ori8834@gmail.com>
2023-03-07 00:42:18 +0200

    dev

--- screens/Login.js
+++ screens/Login.js
@@ -11,9 +11,10 @@ import {
 } from "react-native";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../config/firebase";
-const backImage = require("../assets/I-made-an-illustration-inspired-by-Windows-95-and-90s-anime.png");
 
-export default function Login({ navigation }) {
+const backImage = require("../assets/backImage.png");
+
+export default function Login({ navigration }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -24,4 +25,59 @@ export default function Login({ navigation }) {
         .catch((err) => Alert.alert("Login error", err.message));
     }
   };
+  return (
+    <View style={styles.container}>
+      <Image source={backImage} style={styles.backImage} />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  title: {
+    fontSize: 36,
+    fontWeight: "bold",
+    color: "orange",
+    alignSelf: "center",
+    paddingBottom: 24,
+  },
+  input: {
+    backgroundColor: "#F6F7FB",
+    height: 58,
+    marginBottom: 20,
+    fontSize: 16,
+    borderRadius: 10,
+    padding: 12,
+  },
+  backImage: {
+    width: "100%",
+    height: 340,
+    position: "absolute",
+    top: 0,
+    resizeMode: "cover",
+  },
+  whiteSheet: {
+    width: "100%",
+    height: "75%",
+    position: "absolute",
+    bottom: 0,
+    backgroundColor: "#fff",
+    borderTopLeftRadius: 60,
+  },
+  form: {
+    flex: 1,
+    justifyContent: "center",
+    marginHorizontal: 30,
+  },
+  button: {
+    backgroundColor: "#f57c00",
+    height: 58,
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 40,
+  },
+});
