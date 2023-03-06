e6e21586e10c0a79f28bf3cd21b24000ea3d3035
senior_Ori <ori8834@gmail.com>
2023-03-06 17:47:33 +0200

    dev

---
+++ screens/Login.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  SafeAreaView,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../config/firebase";
+const backImage = require("../assets/I-made-an-illustration-inspired-by-Windows-95-and-90s-anime.png");
+
+export default function Login({ navigation }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onHandleLogin = () => {
+    if (email !== "" && password !== "") {
+      signInWithEmailAndPassword(auth, email, password)
+        .then(() => console.log("Login success"))
+        .catch((err) => Alert.alert("Login error", err.message));
+    }
+  };
+}
