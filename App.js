98ffe61b6501927e1bacb93a32bb79d3b2593ebe
senior_Ori <ori8834@gmail.com>
2023-03-11 05:57:46 +0200

    scratch/rn-guide

--- App.js
+++ App.js
@@ -1,59 +1,76 @@
-import React from "react";
-import { View, Text, TextInput, Button } from "react-native";
-import { auth } from "./config/firebase";
+import React, { useState, useContext, useEffect, createContext } from "react";
+import {
+  View,
+  ActivityIndicator,
+  Text,
+  StyleSheet,
+  Button,
+} from "react-native";
+import messaging from "@react-native-firebase/messaging";
+import { TextInput } from "react-native-gesture-handler";
 
-class Login extends React.Component {
-  state = {
-    email: "",
-    password: "",
-    specialKey: "",
+export default function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [additionalText, setAdditionalText] = useState("");
+
+  const handleEmailInput = (text) => {
+    setEmail(text);
   };
 
-  handleLogin = () => {
-    console.log("auth:", auth); // add this line
-    const { email, password } = this.state;
-    auth
-      .signInWithEmailAndPassword(email, password)
-      .then(() => {
-        // handle successful login here
-      })
-      .catch((error) => {
-        // handle login error here
-      });
+  const handlePasswordInput = (text) => {
+    setPassword(text);
   };
 
-  handleSignup = () => {
-    console.log("auth:", auth); // add this line
-    const { email, password } = this.state;
-    auth
-      .createUserWithEmailAndPassword(email, password)
-      .then(() => {
-        // handle successful signup here
-      })
-      .catch((error) => {
-        // handle signup error here
-      });
+  const handleAdditionalTextInput = (text) => {
+    setAdditionalText(text);
   };
 
-  render() {
-    return (
-      <View>
-        <Text>Email:</Text>
-        <TextInput
-          value={this.state.email}
-          onChangeText={(email) => this.setState({ email })}
-        />
-        <Text>Password:</Text>
-        <TextInput
-          value={this.state.password}
-          onChangeText={(password) => this.setState({ password })}
-          secureTextEntry
-        />
-        <Button title="Login" onPress={this.handleLogin} />
-        <Button title="Signup" onPress={this.handleSignup} />
-      </View>
-    );
-  }
+  const handleSubmit = () => {
+    // do something with the email, password, and additional text
+    console.log(email, password, additionalText);
+  };
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={handleEmailInput}
+        keyboardType="email-address"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        value={password}
+        onChangeText={handlePasswordInput}
+        secureTextEntry={true}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Additional text"
+        value={additionalText}
+        onChangeText={handleAdditionalTextInput}
+      />
+      <Button style={styles.button} title="Submit" onPress={handleSubmit} />
+      {/* <Register /> */}
+    </View>
+  );
 }
-
-export default Login;
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: "10%",
+    flex: 1,
+    marginHorizontal: 20,
+    marginVertical: 10,
+  },
+  input: {
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+  },
+  button: {
+    marginTop: 10,
+  },
+});
