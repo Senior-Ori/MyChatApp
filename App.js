87558aa9d43ca5da6d314c0b7f82a8948edf5969
senior_Ori <ori8834@gmail.com>
2023-03-26 14:17:35 +0300

    dev/added/delete-goals

--- App.js
+++ App.js
@@ -1,61 +1,125 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+import {
+  View,
+  ActivityIndicator,
+  Text,
+  StyleSheet,
+  Button,
+  FlatList,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { TextInput } from "react-native-gesture-handler";
+
+const GoalContext = React.createContext({
+  courseGoals: [],
+  addGoal: (goal) => {},
+  deleteGoal: (goal) => {},
+});
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState("");
   const [courseGoals, setCourseGoals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    retrieveData();
+    async function loadData() {
+      try {
+        const jsonValue = await AsyncStorage.getItem("courseGoals");
+        if (jsonValue !== null) {
+          setCourseGoals(JSON.parse(jsonValue));
+        }
+      } catch (e) {
+        console.log(e);
+      }
+      setIsLoading(false);
+    }
+    loadData();
   }, []);
 
-  const retrieveData = async () => {
-    try {
-      const value = await AsyncStorage.getItem("courseGoals");
-      if (value !== null) {
-        setCourseGoals(JSON.parse(value));
+  useEffect(() => {
+    async function saveData() {
+      try {
+        const jsonValue = JSON.stringify(courseGoals);
+        await AsyncStorage.setItem("courseGoals", jsonValue);
+      } catch (e) {
+        console.log(e);
       }
-    } catch (error) {
-      console.log(error);
     }
-  };
+    saveData();
+  }, [courseGoals]);
 
   function goalInputHandler(enteredText) {
     setEnteredGoalText(enteredText);
   }
 
   function addGoalHandler() {
+    if (enteredGoalText.length === 0) {
+      Alert.alert("Invalid Goal", "Goal must not be empty.", [{ text: "OK" }]);
+      return;
+    }
     setCourseGoals((currentCourseGoals) => [
       ...currentCourseGoals,
       enteredGoalText,
     ]);
-    AsyncStorage.setItem(
-      "courseGoals",
-      JSON.stringify([...courseGoals, enteredGoalText])
-    );
     setEnteredGoalText("");
   }
 
+  function deleteGoalHandler(goal) {
+    setCourseGoals((currentCourseGoals) =>
+      currentCourseGoals.filter((item) => item !== goal)
+    );
+  }
+
+  function renderItem({ item }) {
+    return (
+      <TouchableOpacity
+        onLongPress={() => deleteGoalHandler(item)}
+        activeOpacity={0.7}
+      >
+        <View key={item} style={styles.goalItem}>
+          <Text style={styles.goalText}>{item}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <View style={styles.centered}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
   return (
-    <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.textInput}
-          placeholder="Your course goal!"
-          onChangeText={goalInputHandler}
-          value={enteredGoalText}
-        />
-        <Button title="Add Goal" onPress={addGoalHandler} />
+    <GoalContext.Provider
+      value={{
+        courseGoals: courseGoals,
+        addGoal: addGoalHandler,
+        deleteGoal: deleteGoalHandler,
+      }}
+    >
+      <View style={styles.appContainer}>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.textInput}
+            placeholder="Your course goal!"
+            onChangeText={goalInputHandler}
+            value={enteredGoalText}
+          />
+          <Button title="Add Goal" onPress={addGoalHandler} />
+        </View>
+        <View style={styles.goalsContainer}>
+          <FlatList
+            data={courseGoals}
+            renderItem={renderItem}
+            keyExtractor={(item) => item}
+          />
+        </View>
       </View>
-      <View style={styles.goalsContainer}>
-        {courseGoals.map((goal) => (
-          <View key={goal} style={styles.goalItem}>
-            <Text style={styles.goalText}>{goal}</Text>
-          </View>
-        ))}
-      </View>
-    </View>
+    </GoalContext.Provider>
   );
 }
 
@@ -66,7 +130,6 @@ const styles = StyleSheet.create({
     paddingHorizontal: 16,
   },
   inputContainer: {
-    flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
@@ -89,8 +152,20 @@ const styles = StyleSheet.create({
     padding: 8,
     borderRadius: 6,
     backgroundColor: "#5e0acc",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
   goalText: {
+    color: "white",
+  },
+  deleteButton: {
+    backgroundColor: "red",
+    borderRadius: 6,
+    padding: 8,
+    marginLeft: 8,
+  },
+  deleteButtonText: {
     color: "white",
   },
 });
