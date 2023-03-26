6ead3939387f8db36e7eb4a50de4504a39909337
senior_Ori <ori8834@gmail.com>
2023-03-26 16:43:16 +0300

    dev/backwards-simplify

--- App.js
+++ App.js
@@ -1,125 +1,48 @@
-import React, { useState, useEffect } from "react";
-import {
-  View,
-  ActivityIndicator,
-  Text,
-  StyleSheet,
-  Button,
-  FlatList,
-  TouchableOpacity,
-  Alert,
-} from "react-native";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { TextInput } from "react-native-gesture-handler";
+import React, { useState, useContext, useEffect, createContext } from "react";
+import { View, ActivityIndicator, StyleSheet, FlatList } from "react-native";
+import messaging from "@react-native-firebase/messaging";
 
-const GoalContext = React.createContext({
-  courseGoals: [],
-  addGoal: (goal) => {},
-  deleteGoal: (goal) => {},
-});
+import GoalItem from "./components/GoalItem";
+import GoalInput from "./components/GoalInput";
 
 export default function App() {
-  const [enteredGoalText, setEnteredGoalText] = useState("");
   const [courseGoals, setCourseGoals] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    async function loadData() {
-      try {
-        const jsonValue = await AsyncStorage.getItem("courseGoals");
-        if (jsonValue !== null) {
-          setCourseGoals(JSON.parse(jsonValue));
-        }
-      } catch (e) {
-        console.log(e);
-      }
-      setIsLoading(false);
-    }
-    loadData();
-  }, []);
-
-  useEffect(() => {
-    async function saveData() {
-      try {
-        const jsonValue = JSON.stringify(courseGoals);
-        await AsyncStorage.setItem("courseGoals", jsonValue);
-      } catch (e) {
-        console.log(e);
-      }
-    }
-    saveData();
-  }, [courseGoals]);
-
-  function goalInputHandler(enteredText) {
-    setEnteredGoalText(enteredText);
-  }
-
-  function addGoalHandler() {
-    if (enteredGoalText.length === 0) {
-      Alert.alert("Invalid Goal", "Goal must not be empty.", [{ text: "OK" }]);
-      return;
-    }
+  function addGoalHandler(enteredGoalText) {
     setCourseGoals((currentCourseGoals) => [
       ...currentCourseGoals,
-      enteredGoalText,
+      { text: enteredGoalText, id: Math.random().toString() },
     ]);
-    setEnteredGoalText("");
   }
 
-  function deleteGoalHandler(goal) {
-    setCourseGoals((currentCourseGoals) =>
-      currentCourseGoals.filter((item) => item !== goal)
-    );
-  }
-
-  function renderItem({ item }) {
-    return (
-      <TouchableOpacity
-        onLongPress={() => deleteGoalHandler(item)}
-        activeOpacity={0.7}
-      >
-        <View key={item} style={styles.goalItem}>
-          <Text style={styles.goalText}>{item}</Text>
-        </View>
-      </TouchableOpacity>
-    );
-  }
-
-  if (isLoading) {
-    return (
-      <View style={styles.centered}>
-        <ActivityIndicator size="large" />
-      </View>
-    );
+  function deleteGoalHandler(id) {
+    setCourseGoals((currentCourseGoals) => {
+      return currentCourseGoals.filter((goal) => goal.id !== id);
+    });
   }
 
   return (
-    <GoalContext.Provider
-      value={{
-        courseGoals: courseGoals,
-        addGoal: addGoalHandler,
-        deleteGoal: deleteGoalHandler,
-      }}
-    >
-      <View style={styles.appContainer}>
-        <View style={styles.inputContainer}>
-          <TextInput
-            style={styles.textInput}
-            placeholder="Your course goal!"
-            onChangeText={goalInputHandler}
-            value={enteredGoalText}
-          />
-          <Button title="Add Goal" onPress={addGoalHandler} />
-        </View>
-        <View style={styles.goalsContainer}>
-          <FlatList
-            data={courseGoals}
-            renderItem={renderItem}
-            keyExtractor={(item) => item}
-          />
-        </View>
+    <View style={styles.appContainer}>
+      <GoalInput onAddGoal={addGoalHandler} />
+      <View style={styles.goalsContainer}>
+        <FlatList
+          data={courseGoals}
+          renderItem={(itemData) => {
+            return (
+              <GoalItem
+                text={itemData.item.text}
+                id={itemData.item.id}
+                onDeleteItem={deleteGoalHandler}
+              />
+            );
+          }}
+          keyExtractor={(item, index) => {
+            return item.id;
+          }}
+          alwaysBounceVertical={false}
+        />
       </View>
-    </GoalContext.Provider>
+    </View>
   );
 }
 
@@ -129,43 +52,28 @@ const styles = StyleSheet.create({
     padding: 50,
     paddingHorizontal: 16,
   },
-  inputContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: "#ccc",
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: "#ccc",
-    width: "70%",
-    marginRight: 8,
-    padding: 8,
-  },
+
   goalsContainer: {
     flex: 4,
   },
-  goalItem: {
-    margin: 8,
-    padding: 8,
-    borderRadius: 6,
-    backgroundColor: "#5e0acc",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-  },
-  goalText: {
-    color: "white",
-  },
-  deleteButton: {
-    backgroundColor: "red",
-    borderRadius: 6,
-    padding: 8,
-    marginLeft: 8,
-  },
-  deleteButtonText: {
-    color: "white",
-  },
+
+  // container: {
+  //   paddingTop: "10%",
+  //   flex: 1,
+  //   marginHorizontal: 20,
+  //   marginVertical: 10,
+  // },
+  // input: {
+  //   borderWidth: 1,
+  //   borderRadius: 5,
+  //   padding: 10,
+  //   marginBottom: 10,
+  // },
+  // button: {
+  //   marginTop: 10,
+  // },
+  // dummyText: { margin: 16, padding: 16, borderWidth: 2, borderColor: "blue" },
+
+  /*
+   */
 });
---
+++ components/GoalInput.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { StyleSheet, View, Button } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+
+function GoalInput(props) {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+
+  function addGoalHandler() {
+    props.onAddGoal(enteredGoalText);
+    setEnteredGoalText("");
+  }
+
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.textInput}
+        placeholder="Your course goal!"
+        onChangeText={goalInputHandler}
+        value={enteredGoalText}
+      />
+      <Button title="Add Goal" onPress={addGoalHandler} />
+    </View>
+  );
+}
+
+export default GoalInput;
+const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+  },
+});
