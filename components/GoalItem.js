dfe6704fb56baeaf6c2941818e2dc9085b8db08e
senior_Ori <ori8834@gmail.com>
2023-03-26 22:04:32 +0300

    app/finished-beta

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -3,20 +3,31 @@ import { StyleSheet, View, Text, Pressable } from "react-native";
 
 function GoalItem(props) {
   const [unixTime, setUnixTime] = useState(0);
-
+  const [timeDifference, setTimeDifference] = useState("");
   useEffect(() => {
-    const interval = setInterval(() => {
+    const intervalId = setInterval(() => {
       fetch(
-        `https://ori-projects-default-rtdb.europe-west1.firebasedatabase.app/esp32project/sensor${props.text}/1.json`
+        `https://ori-projects-default-rtdb.europe-west1.firebasedatabase.app/esp32project/sensor${props.text}/1/unixtime.json`
       )
         .then((response) => response.json())
-        .then((data) => setUnixTime(data.unixtime));
+        .then((data) => {
+          const currentTime = Math.floor(Date.now() / 1000);
+
+          const difference = currentTime - data;
+          const hours = Math.floor(difference / 3600);
+          const minutes = Math.floor((difference % 3600) / 60);
+          const seconds = difference % 60;
+          setTimeDifference(
+            `${hours} hours, ${minutes} minutes, ${seconds} seconds`
+          );
+          console.log(
+            `\ncurrent unixtime:${currentTime}\nperiod unixtime:${difference}\nperiod time:${hours}:${minutes}:${seconds}\n`
+          );
+        })
+        .catch((error) => console.error(error));
     }, 1000);
-
-    return () => clearInterval(interval);
+    return () => clearInterval(intervalId);
   }, []);
-
-  // const counter = Math.floor(unixTime / 1000);
 
   return (
     <View style={styles.goalItem}>
@@ -25,7 +36,7 @@ function GoalItem(props) {
         onPress={props.onDeleteItem.bind(this, props.id)}
         style={({ pressed }) => pressed && styles.pressedItem}
       >
-        <Text style={styles.goalText}>{Date.now() - unixTime} seconds</Text>
+        <Text style={styles.goalText}>{timeDifference}</Text>
       </Pressable>
     </View>
   );
